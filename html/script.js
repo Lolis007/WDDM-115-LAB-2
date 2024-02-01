4e3cb2cdf829be36a9b3d96cbc3514f93bd24224
Lolis007 <ladan.aisha@yahoo.com>
2024-02-01 11:21:11 -0500

    change background color button added

---
+++ html/script.js
@@ -0,0 +1,13 @@
+function changeBackgroundColor(){
+    var body = document.body;
+    body.style.backgroundColor = 'white'; 
+}
+
+
+/*function changeBackgroundColor() {
+var body = document.body*/
+/* changes background color continuously
+var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+body.style.backgroundColor = randomColor;*/
+
+/*body.style.backgroundColor = "white";*/
